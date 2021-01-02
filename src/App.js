4c6caeca849857866ceb5ff50ab6f128dfa87f5b
sadiafarooq220 <72773763+sadiafarooq220@users.noreply.github.com>
2021-01-02 18:38:37 +0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Modified by Majd Uddin <br />
+          Modified by Sadia Farooq <br />
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
